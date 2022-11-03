403d3b3cd31e29aac21b903202a6ca5b745e30ba
Srpapaleta <95599570+Srpapaleta@users.noreply.github.com>
2022-11-02 20:27:18 -0500

    Add files via upload

---
+++ Filters.jsx
@@ -0,0 +1,12 @@
+function Filters(){
+    return <div>
+        <button>Drama</button>
+        <button>Horror</button>
+        <button>Arte</button>
+        <button>Bibliografía</button>
+        <button>Misterio</button>
+        <button>Ficción</button>
+        <button>Deportes</button>
+        <button>Aventura</button>
+    </div>
+}
